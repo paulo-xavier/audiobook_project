2b396097bf89a249bd4ea660df5013696fd11944
paulo-xavier <px568661@gmail.com>
2024-02-01 01:16:43 -0300

    :rocket: Completing Javascript and deploying

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const buttonPlayPause = document.getElementById('play-pause');
+const nextButton = document.getElementById('next');
+const buttonReturn = document.getElementById('previous'); 
+const chapterName = document.getElementById('chapter');
 const audioChapter = document.getElementById('chapter-audio');
 
 const numberChapters = 10;
 let isPlaying = 0; // 1 - playing , 0 - paused
+let currentChapter = 1; 
 
 function playTrack() {
     audioChapter.play();
@@ -27,5 +31,40 @@ function playOrPause() {
     }
 }
 
+function changeTrackName() {
+    chapterName.innerText = `Chapter ${currentChapter}`; 
+}
+
+function nextTrack() {
+    if (currentChapter === numberChapters) {
+        currentChapter = 1; 
+
+    } else {
+        currentChapter += 1;
+    }
+
+    audioChapter.src = `./books/dom-casmurro/${currentChapter}.mp3`;
+    playTrack();
+    isPlaying = 1;
+    changeTrackName();
+}
+
+function returnTrack() {
+    if (currentChapter === 1) {
+        currentChapter = numberChapters; 
+
+    } else {
+        currentChapter -= 1;
+    }
+
+    audioChapter.src = `./books/dom-casmurro/${currentChapter}.mp3`;
+    playTrack();
+    isPlaying = 1;
+    changeTrackName();
+}
+
 buttonPlayPause.addEventListener("click", playOrPause);
 //buttonPlayPause.onclick(playTrack)
+
+nextButton.addEventListener('click', nextTrack); 
+buttonReturn.addEventListener('click', returnTrack);
